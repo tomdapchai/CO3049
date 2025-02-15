bf6e0ad08dd096f4a2509d8a1d4dc2a5d337f94c
tomdapchai <vothanhtam2407@gmail.com>
2025-02-15 23:06:21 +0700

    i have no idea

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
         ],
         domains: ["res.cloudinary.com"],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
